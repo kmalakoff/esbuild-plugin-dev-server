db2f8c115054578563efc181b9147e6988d91400
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-31 13:27:16 -0700

    Comprehensive types

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -6,7 +6,7 @@ import endpoint from 'react-dev-utils/launchEditorEndpoint.js';
 const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
 
 let string = null;
-export default function client() {
+export default function client(): string {
   if (string) return string;
   string = fs.readFileSync(path.join(__dirname, '..', '..', '..', 'assets', 'client.cjs'), 'utf8');
   string = string.replace(`require('react-dev-utils/launchEditorEndpoint')`, `'${endpoint}'`);
