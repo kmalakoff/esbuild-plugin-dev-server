001f14298b0f104a2162930fc339258609db3e9d
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-20 23:52:58 -0700

    Upgrade modules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import plugin from './lib/plugin.js';
+import plugin from './lib/plugin.ts';
 export default plugin;
-export { default as client } from './lib/client.js';
-export { default as socketServer } from './lib/socketServer.js';
-export * from './types.js';
+export { default as client } from './lib/client.ts';
+export { default as socketServer } from './lib/socketServer.ts';
+export * from './types.ts';
