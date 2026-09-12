95cd53c5c3da2663e1503f010bd96f245a955f98
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-11 18:22:42 -0700

    Upgrade modules

---
+++ test/exports/import.test.mjs
@@ -0,0 +1,16 @@
+import { test } from '@playwright/test';
+
+import assert from 'assert';
+import plugin, { client, socketServer } from 'esbuild-plugin-dev-server';
+
+test.describe('exports .mjs', () => {
+  test('default', () => {
+    assert.equal(typeof plugin, 'function');
+  });
+  test('client', () => {
+    assert.equal(typeof client, 'function');
+  });
+  test('socketServer', () => {
+    assert.equal(typeof socketServer, 'function');
+  });
+});
